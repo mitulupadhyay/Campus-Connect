d9049e33e7064e4ff01b39cf497bfdf4ca9cf8ec
mitul upadhyay <mitulupadhyay0910@gmail.com>
2026-07-04 09:30:34 +0530

    Fix navbar issues and improve upcoming events layout

--- frontend/pages/js/home.js
+++ frontend/pages/js/home.js
@@ -1,6 +1,13 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
 
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+navItems.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
 });
